de9e26eec7c36e48446feec8426639e13cd3366f
gocha <gochaism@gmail.com>
2026-03-02 22:34:54 +0900

    snes9x_breakpoints.lua: Add read/write callback functions

--- Misc/snes9x_breakpoints.lua
+++ Misc/snes9x_breakpoints.lua
@@ -3,8 +3,32 @@
 
 -- Callbacks
 function on_execute(addr, size)
-    print(string.format("Frame %d | $%06X executed.", emu.framecount(), addr))
+    print(string.format("Frame %d | Execute at $%06X.", emu.framecount(), addr))
+end
+
+function on_read(addr, size)
+    local pc = memory.getregister("pbpc")
+    local value
+    if size == 2 then
+        value = memory.readword(addr)
+    else
+        value = memory.readbyte(addr)
+    end
+    print(string.format("Frame %d | Read $%06X at $%06X (%d byte(s)) = $%X.", emu.framecount(), addr, pc, size, value))
+end
+
+function on_write(addr, size)
+    local pc = memory.getregister("pbpc")
+    local value
+    if size == 2 then
+        value = memory.readword(addr)
+    else
+        value = memory.readbyte(addr)
+    end
+    print(string.format("Frame %d | Write $%06X at $%06X (%d byte(s)) = $%X.", emu.framecount(), addr, pc, size, value))
 end
 
 -- Define breakpoints
-memory.registerexec(0x80952F, on_execute)
+memory.registerexec(0x83D22C, on_execute) -- damage
+memory.registerread(0x7E044E, on_read)    -- subweapon charge
+memory.registerwrite(0x7E008E, on_write)  -- room number
