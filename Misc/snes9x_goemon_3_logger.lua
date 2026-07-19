4fba81d50c60199e8019562976f1464d17295ed9
gocha <gochaism@gmail.com>
2026-07-19 17:04:15 +0900

    Lua: Update asset description

--- Misc/snes9x_goemon_3_logger.lua
+++ Misc/snes9x_goemon_3_logger.lua
@@ -6,11 +6,25 @@ memory.registerexec(0x84BBB0, function(addr, size)
 
     local asset = memory.getregister("x") + 0x880000
     local asset_type = memory.readbyte(asset)
+
     local asset_type_str = "Type " .. asset_type
     if asset_type == 0 then
         asset_type_str = "VRAM"
+    elseif asset_type == 1 then
+        asset_type_str = "WRAM"
     elseif asset_type == 2 then
         asset_type_str = "SPC"
     end
-    print(string.format("%06d | Load asset $%06X (%s) from $%06X.", emu.framecount(), asset, asset_type_str, caller))
+
+    local asset_transfer_mode_str = ""
+    if asset_type ~= 255 then
+        local asset_transfer_mode = memory.readbyte(asset + 1)
+
+        asset_transfer_mode_str = ", mode=" .. asset_transfer_mode
+        if asset_transfer_mode == 0 then
+            asset_transfer_mode_str = ""
+        end
+    end
+
+    print(string.format("%06d | Load asset $%06X (%s%s) from $%06X.", emu.framecount(), asset, asset_type_str, asset_transfer_mode_str, caller))
 end)
