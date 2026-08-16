6f03e18f94f6c128bb8b673e77b20ff09caa4b3a
gocha <gochaism@gmail.com>
2026-08-16 22:05:01 +0900

    RNG Hook: Add support for ROM revision 0 and 2

--- Lua/Ganbare Goemon 3 - RNG Hook.lua
+++ Lua/Ganbare Goemon 3 - RNG Hook.lua
@@ -1,12 +1,22 @@
 -- Ganbare Goemon 3 RNG Research Script
 
+revision = memory.readbyte(0x80FFDB)
+
+rng_advance_fp = 0x86C43D
+if revision == 0 then
+    rng_advance_fp = 0x86C434
+elseif revision == 2 then
+    rng_advance_fp = 0x86C463
+end
+
 local readExcludePCs = {
   0x8080EF, -- idle loop
 
-  -- 0x86C43D, -- part of $86C43D, RNG update routine
-  0x86C440, -- part of $86C43D, RNG update routine
-  0x86C445, -- part of $86C43D, RNG update routine
-  0x86C44A, -- part of $86C43D, RNG update routine
+  -- rng_advance_fp, -- part of $86C43D, RNG update routine
+  rng_advance_fp+3, -- part of $86C43D, RNG update routine
+  rng_advance_fp+8, -- part of $86C43D, RNG update routine
+  rng_advance_fp+13, -- part of $86C43D, RNG update routine
+
   -- 0x86C45B, -- related to generating a new sprite?
   -- 0x8ABF77, -- panic stage #1: background object position?
   -- 0x8AC3B2, -- panic stage #1: target character for spiked bar
@@ -23,7 +33,7 @@ event.onmemoryread(function()
     end
   end
 
-  if pc == 0x86C43D then
+  if pc == rng_advance_fp then
     local s = emu.getregister("S")
     local caller_pc = mainmemory.read_u24_le(s + 1)
     print(string.format("[%d] RNG Advance from $%06X", emu.framecount(), caller_pc))
